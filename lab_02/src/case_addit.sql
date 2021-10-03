f26ede191b3052f9d4566e8436d6ced902e8084d
ArtemevIlya <clayevenson@gmail.com>
2021-10-03 18:56:58 +0300

    insert construct changed

--- lab_02/src/case_addit.sql
+++ lab_02/src/case_addit.sql
@@ -31,45 +31,21 @@ CREATE TEMP TABLE Table2
 INSERT INTO Table1 
     (id, var1, valid_from_dttm, valid_to_dttm)
 VALUES
-    (1, 'A', '2018-09-01', '2018-09-15')
-;
-INSERT INTO Table1 
-    (id, var1, valid_from_dttm, valid_to_dttm)
-VALUES
-    (1, 'B', '2018-09-16', '5999-12-31')
-;
-INSERT INTO Table1 
-    (id, var1, valid_from_dttm, valid_to_dttm)
-VALUES
-    (2, 'A', '2018-09-01', '2018-09-15')
-;
-INSERT INTO Table1 
-    (id, var1, valid_from_dttm, valid_to_dttm)
-VALUES
-    (2, 'B', '2018-09-16', '5999-12-31')
+      (1, 'A', '2018-09-01', '2018-09-15')
+	, (1, 'B', '2018-09-16', '5999-12-31')
+	, (2, 'A', '2018-09-01', '2018-09-15')
+	, (2, 'B', '2018-09-16', '5999-12-31')
 ;
 
 -- Вставка данных во вторую таблицу
-    INSERT INTO Table2
-        (id, var2, valid_from_dttm, valid_to_dttm)
-    VALUES
-        (1, 'A', '2018-09-01', '2018-09-18')
-    ;
-    INSERT INTO Table2
-        (id, var2, valid_from_dttm, valid_to_dttm)
-    VALUES
-        (1, 'B', '2018-09-19', '5999-12-31')
-    ;
-    INSERT INTO Table2
-        (id, var2, valid_from_dttm, valid_to_dttm)
-    VALUES
-        (2, 'A', '2018-09-01', '2018-09-18')
-    ;
-    INSERT INTO Table2
-        (id, var2, valid_from_dttm, valid_to_dttm)
-    VALUES
-        (2, 'B', '2018-09-19', '5999-12-31')
-    ;
+INSERT INTO Table2
+    (id, var2, valid_from_dttm, valid_to_dttm)
+VALUES
+      (1, 'A', '2018-09-01', '2018-09-18')
+	, (1, 'B', '2018-09-19', '5999-12-31')
+	, (2, 'A', '2018-09-01', '2018-09-18')
+	, (2, 'B', '2018-09-19', '5999-12-31')
+;
 
 -- ОТВ
 WITH
